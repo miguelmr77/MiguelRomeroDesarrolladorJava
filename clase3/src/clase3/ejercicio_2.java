14a39059d5727eda06859dc22dd0df1f02d8f719
miguelmr77 <miguel.ar@outlook.com>
2023-03-24 18:10:43 -0300

    ulimamod

--- clase3/src/clase3/ejercicio_2.java
+++ clase3/src/clase3/ejercicio_2.java
@@ -1,73 +1,77 @@
 package clase3;
-import java.util.Scanner;
+
 public class ejercicio_2 {
 	public static void main(String[] args) {
 		
 		
 		System.out.println("clase 3 ejercicio 2b");
 		
+		String Palabra = "hola que tal";
+		int Desplaza = 1;
+		String aux=null;
+		String Vaux[] = new String[12] ;
 		
-		
-		
-			Scanner word = new Scanner(System.in);
-			System.out.println("ingrese palabra");
-			String Palabra = word.nextLine();
-				
-				
-				
-			System.out.println ("ingrese el desplazamiento");
-			Scanner Desplazar = new Scanner (System.in);
-			Integer Desplaza = Integer.parseInt (Desplazar.nextLine());
-
-
 				char abecedario[]= new char [] {' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'};
-
-				System.out.println("Palabra Original: " + Palabra );
-					
+				System.out.println("Palabra Original: " );
+				System.out.println(Palabra );
+				System.out.println();
 						for(int indice =0; indice<Palabra.length(); indice++)
 							
 						{	
-							
 							char Caracter =Palabra.charAt(indice);
+							
 
 							for(int i =0; i<abecedario.length; i++)
 							{
 								
 								if(Caracter==abecedario[i] && ((i + Desplaza)<27))
-								{
-									String c= Caracter + "";
-									String s = abecedario[i+Desplaza] +"";
-									Palabra=Palabra.replaceFirst(c, s);
+								{	
+									aux = abecedario[i+Desplaza] + "";
+									
+										Vaux[indice] = aux;
+									
+									
 								}
+								
 							}
 							
+
+							
 						}
-			
+						System.out.println("Palabra Codificada: ");		
+				for (int i=0; i<Vaux.length; i++) {
+				System.out.print( Vaux[i]);
+				}
+				System.out.println();
+				System.out.println();
 				
-				System.out.println("Palabra Codificada: " + Palabra );
 				
 				
 				
-				
-				for(int indice =0; indice<Palabra.length(); indice++)
+				//decodificar palabra
+				for(int indice =0; indice<Vaux.length; indice++)
 					
 				{	
 					
-					char Caracter =Palabra.charAt(indice);
+					String Caracter = Vaux[indice];
 
 					for(int i =0; i<abecedario.length; i++)
 					{
 						
-						if(Caracter==abecedario[i] && ((i + Desplaza)<27))
+						if(Caracter.equals(abecedario[i]+ "") && ((i + Desplaza)<27))
 						{
-							String c= Caracter + "";
-							String s = abecedario[i-Desplaza] +"";
-							Palabra=Palabra.replaceFirst(c, s);
+							aux = abecedario[i-Desplaza] + "";
+									
+										Vaux[indice] = aux;
 						}
 					}
 					
 				}
 				
-				System.out.println("Palabra Decoodificada: " + Palabra );
+				System.out.println("Palabra Decoodificada: ");
+				for (int i=0; i<Vaux.length; i++) {
+				System.out.print( Vaux[i]);
+				}
+				System.out.println(); 
 			}
 }	
